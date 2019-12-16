10a9d016c14d539c8ba8bcc4726b523fea89a58d
komal1824 <komalrajpal24@gmail.com>
2019-12-16 17:02:51 +0530

    project structure modified

--- conf.js
+++ conf.js
@@ -1,3 +1,4 @@
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 exports.config = {
     framework : 'jasmine',
     specs : ['./T1_nbcNavigationCheck.js'],
@@ -10,6 +11,17 @@ exports.config = {
             browser.manage().window().maximize()
             browser.manage().timeouts().implicitlyWait(90000)
 
+    },
+
+    //for screenshots
+    onPrepare : function()
+    {
+      browser.driver.manage().window().maximize();
+      jasmine.getEnv().addReporter(
+          new Jasmine2HtmlReporter({
+              savePath: 'target/screenshots'
+          })
+      )
     }
     //for chrome cababilities need not be defined.
     //for firefox and IE . capabilities should b e defined in this config file.
